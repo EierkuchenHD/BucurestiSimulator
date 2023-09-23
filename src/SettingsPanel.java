e66c256379843eb9818b9f5881df8c86b03257f8
EierkuchenHD <eierkuchenhd@gmail.com>
2023-09-23 11:57:26 +0200

    removed unnecessary import

--- src/SettingsPanel.java
+++ src/SettingsPanel.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.prefs.Preferences;
 
