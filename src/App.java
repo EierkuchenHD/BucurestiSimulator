ad0770d73fbf7321c8419f548b18a7cc652caf8a
EierkuchenHD <eierkuchenhd@gmail.com>
2023-09-23 10:41:11 +0200

    Added error handling to the App class and changed jdk version to 11

--- src/App.java
+++ src/App.java
@@ -1,7 +1,15 @@
 import javax.swing.SwingUtilities;
+import javax.swing.JOptionPane;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        SwingUtilities.invokeLater(() -> new MainMenu());
+    public static void main(String[] args) {
+        try {
+            SwingUtilities.invokeLater(() -> new MainMenu());
+        } catch (Exception e) {
+            // Display a detailed error message if an exception occurs
+            String errorMessage = "An error occurred while starting the game:\n" + e.getMessage();
+            JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace(); // Print the stack trace for debugging purposes
+        }
     }
 }
