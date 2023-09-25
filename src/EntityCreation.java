7217b349c05dd8dcb8d31201d12a403d947aab7b
EierkuchenHD <eierkuchenhd@gmail.com>
2023-09-25 21:19:32 +0200

    basic darius now disappears when clicked

--- src/EntityCreation.java
+++ src/EntityCreation.java
@@ -1,14 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.util.Random;
 
 public class EntityCreation {
     public static void createEntityPanel(JPanel parentPanel) {
         EntityPanel entityPanel = new EntityPanel();
         parentPanel.add(entityPanel, BorderLayout.CENTER);
-        parentPanel.revalidate(); // Ensure layout updates
     }
 }
 
@@ -22,36 +20,54 @@ class EntityPanel extends JPanel {
         entityIcon = new ImageIcon("images/basicdarius.png");
 
         // Initialize the timer
-        timer = new Timer(1000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (entityVisible) {
-                    SFX.playSecretSound();
-                    // Generate random coordinates
-                    x = new Random().nextInt(getWidth() - 140) + 20;
-                    y = new Random().nextInt(getHeight() - 140) + 20;
-                }
-                repaint(); // Repaint the panel to show the updated entity position
+        timer = new Timer(1000, e -> {
+            if (entityVisible) {
+                SFX.playSecretSound();
+                generateRandomCoordinates();
             }
+            repaint(); // Repaint the panel to show the updated entity position
         });
         timer.setRepeats(true);
         timer.start();
+
+        // Add a MouseListener to handle image click events
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (entityVisible && isClickInsideEntity(e.getX(), e.getY())) {
+                    entityVisible = false;
+                    repaint(); // Repaint to hide the entity
+                }
+            }
+        });
+    }
+
+    private void generateRandomCoordinates() {
+        x = new Random().nextInt(getWidth() - 140) + 20;
+        y = new Random().nextInt(getHeight() - 140) + 20;
+    }
+
+    private boolean isClickInsideEntity(int clickX, int clickY) {
+        return clickX >= x && clickX <= x + 100 && clickY >= y && clickY <= y + 100;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Generate random initial coordinates only once when painting for the first
-        // time
-        if (x == 0 && y == 0) {
-            x = new Random().nextInt(getWidth() - 140) + 20;
-            y = new Random().nextInt(getHeight() - 140) + 20;
-        }
-
         if (entityVisible) {
+            if (x == 0 && y == 0) {
+                generateRandomCoordinates();
+            }
             // Draw the image at the current coordinates (x, y) with a size of 100x100
             g.drawImage(entityIcon.getImage(), x, y, 100, 100, this);
         }
+    }
+}
+
+class SFX {
+    public static void playSecretSound() {
+        // Implement your sound playback logic here
+        // For simplicity, we'll leave it empty in this example
     }
 }
