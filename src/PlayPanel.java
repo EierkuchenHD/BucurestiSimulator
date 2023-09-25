d8a8f642cda6afce77acbe2dc431f993ab9d9a5b
EierkuchenHD <eierkuchenhd@gmail.com>
2023-09-25 21:43:29 +0200

    improved organization of the playpanel class

--- src/PlayPanel.java
+++ src/PlayPanel.java
@@ -11,43 +11,49 @@ public class PlayPanel extends JPanel {
     private Timer countdownTimer;
 
     public PlayPanel() {
+        initializeComponents();
+        createLabels();
+        setupCountdownTimer();
+    }
+
+    private void initializeComponents() {
+        countdownLabel = new JLabel("Get ready in " + countdownSeconds + " seconds");
+        countdownLabel.setFont(new Font("Consolas", Font.BOLD, 24));
+
+        timeLeftLabel = new JLabel("Time Left: 0s");
+        timeLeftLabel.setFont(new Font("Consolas", Font.BOLD, 24));
+
+        robberiesLabel = new JLabel("Robberies: 0");
+        robberiesLabel.setFont(new Font("Consolas", Font.BOLD, 24));
+    }
+
+    private void createLabels() {
         setLayout(new BorderLayout());
 
-        // Create a panel for the top center
         JPanel topCenterPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbcCenter = new GridBagConstraints();
         gbcCenter.insets = new Insets(5, 10, 5, 10); // Padding
-
-        countdownLabel = new JLabel("Get ready in " + countdownSeconds + " seconds");
-        countdownLabel.setFont(new Font("Consolas", Font.BOLD, 24));
         topCenterPanel.add(countdownLabel, gbcCenter);
 
-        // Add the top center panel to the main panel
-        add(topCenterPanel, BorderLayout.CENTER);
-
-        // Create a panel for the top left corner
         JPanel topLeftPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbcLeft = new GridBagConstraints();
         gbcLeft.insets = new Insets(5, 10, 5, 10); // Padding
-        timeLeftLabel = new JLabel("Time Left: 0");
-        timeLeftLabel.setFont(new Font("Consolas", Font.BOLD, 24));
         topLeftPanel.add(timeLeftLabel, gbcLeft);
 
-        // Create a panel for the top right corner
         JPanel topRightPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbcRight = new GridBagConstraints();
         gbcRight.insets = new Insets(5, 10, 5, 10); // Padding
-        robberiesLabel = new JLabel("Robberies: 0");
-        robberiesLabel.setFont(new Font("Consolas", Font.BOLD, 24));
         topRightPanel.add(robberiesLabel, gbcRight);
 
-        // Add the top left and top right panels to the main panel
         JPanel topPanel = new JPanel(new BorderLayout());
         topPanel.add(topLeftPanel, BorderLayout.WEST);
         topPanel.add(topRightPanel, BorderLayout.EAST);
-        add(topPanel, BorderLayout.NORTH);
 
-        // Create a timer to update the countdown
+        add(topPanel, BorderLayout.NORTH);
+        add(topCenterPanel, BorderLayout.CENTER);
+    }
+
+    private void setupCountdownTimer() {
         countdownTimer = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -57,17 +63,17 @@ public class PlayPanel extends JPanel {
                     countdownLabel.setText("Get ready in " + countdownSeconds + " seconds");
                 } else if (countdownSeconds == 0) {
                     countdownLabel.setText("GO!!!");
-
-                    // Start a timer to spawn Basic Dariuses after the countdown
-                    EntityCreation.createEntityPanel(PlayPanel.this);
+                    spawnEntity();
                 } else {
-                    // Timer reached a negative value (countdown finished)
-                    ((Timer) e.getSource()).stop(); // Stop the timer
-                    countdownLabel.setText(""); // Clear the label
+                    ((Timer) e.getSource()).stop();
+                    countdownLabel.setText("");
                 }
             }
         });
+        countdownTimer.start();
+    }
 
-        countdownTimer.start(); // Start the countdown timer
+    private void spawnEntity() {
+        EntityCreation.createEntityPanel(PlayPanel.this);
     }
 }
